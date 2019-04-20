8a6b6497b87a8587b13415b4aac86cb2685eaaa0
Varun PIlakavil <varun.pilakavil@gmail.com>
2019-04-21 01:29:49 +0530

    Updated 5 Assignments
    Added new and better codes of first 5 assignments

--- Assignment2/index.js
+++
@@ -1,7 +0,0 @@
-function toggle(obj) {
-    var item = document.getElementById(obj);
-    if (item.style.visibility == 'visible')
-        item.style.visibility = 'hidden';
-    else
-        item.style.visibility = 'visible';
-}
